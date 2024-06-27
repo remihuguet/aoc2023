57ec9e5b24801e5b0afd03343eac173d6778e3a5
Rémi Huguet <remi.huguet@gmail.com>
2024-06-27 15:27:33 +0200

    8

--- 8/test.py
+++ 8/test.py
@@ -2,11 +2,33 @@ import wasteland
 
 
 def test_parse_file():
+    assert wasteland.parse_file("8/example_2.txt") == (
+        "LLR",
+        {
+            "AAA": wasteland.Node("AAA", l="BBB", r="BBB"),
+            "BBB": wasteland.Node("BBB", l="AAA", r="ZZZ"),
+            "ZZZ": wasteland.Node("ZZZ", l="ZZZ", r="ZZZ"),
+        },
+    )
+
     assert wasteland.parse_file("8/example.txt") == (
         "RL",
-        [
-            wasteland.Node("AAA", l="BBB", r="BBB"),
-            wasteland.Node("BBB", l="AAA", r="ZZZ"),
-            wasteland.Node("ZZZ", l="ZZZ", r="ZZZ"),
-        ],
+        {
+            "AAA": wasteland.Node("AAA", l="BBB", r="CCC"),
+            "BBB": wasteland.Node("BBB", l="DDD", r="EEE"),
+            "CCC": wasteland.Node("CCC", l="ZZZ", r="GGG"),
+            "DDD": wasteland.Node("DDD", l="DDD", r="DDD"),
+            "EEE": wasteland.Node("EEE", l="EEE", r="EEE"),
+            "GGG": wasteland.Node("GGG", l="GGG", r="GGG"),
+            "ZZZ": wasteland.Node("ZZZ", l="ZZZ", r="ZZZ"),
+        },
     )
+
+
+def test_compute_steps_to_zzz():
+    assert wasteland.compute_steps_to_zzz("8/example.txt") == 2
+    assert wasteland.compute_steps_to_zzz("8/example_2.txt") == 6
+
+
+def test_compute_paralell():
+    assert wasteland.compute_steps_to_xxz("8/example_3.txt") == 6
--- 8/wasteland.py
+++ 8/wasteland.py
@@ -1,9 +1,38 @@
+from collections import deque
+import dataclasses
+
+
+@dataclasses.dataclass(frozen=True)
 class Node:
-    def __init__(self, name: str, l: str, r: str):
-        self.name = name
-        self.left = l
-        self.right = r
+    name: str
+    l: str
+    r: str
 
 
 def parse_file(filename: str) -> tuple[str, list[Node]]:
-    pass
+    with open(filename) as f:
+        lines = f.readlines()
+        instructions = lines[0].strip()
+        nodes = {}
+        for line in lines[2:]:
+            name, _, raw_n = line.strip().partition(" = ")
+            l, r = (n.strip() for n in raw_n.strip("()").split(","))
+            nodes[name] = Node(name, l, r)
+    return instructions, nodes
+
+
+def compute_steps_to_zzz(filename: str) -> int:
+    instructions, nodes = parse_file(filename)
+    current_node = nodes["AAA"]
+    steps = 0
+    inst = instructions[0]
+    while current_node.name != "ZZZ":
+        current_node = nodes[current_node.l] if inst == "L" else nodes[current_node.r]
+        steps += 1
+        inst = instructions[steps % len(instructions)]
+    return steps
+
+
+def compute_steps_to_xxz(filename: str) -> int:
+    instructions, nodes = parse_file(filename)
+    starting_nodes = [n for n in nodes.values() if n.name.startswith("A")]
